18de028066d13de9e7961db685bb5b628d6de278
Jan Beck <delite.jcb@googlemail.com>
2014-05-22 02:39:59 +0200

    added import page

--- views/admin.php
+++ views/admin.php
@@ -13,10 +13,61 @@
  */
 ?>
 
-<div class="wrap">
+<div class="tribe_settings wrap">
 
 	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
 
-	<!-- @TODO: Provide markup for your options page here. -->
+	<?php if ( !empty( $this->errors ) ) : ?>
+		<div class="error">
+			<p><strong><?php _e( 'The following errors have occurred:', $this->plugin_slug ); ?></strong></p>
+			<ul class="admin-list">
+				<?php foreach ( $this->errors as $error ) : ?>
+					<li><?php echo $error; ?></li>
+				<?php endforeach; ?>
+			</ul>
+			<?php if ( $this->no_events_imported ) : ?>
+				<p><?php _e( 'Please note that as a result, no events were successfully imported.', $this->plugin_slug ); ?></p>
+			<?php else : ?>
+				<p><?php _e( 'Please note that other events have been successfully imported.', $this->plugin_slug ); ?></p>
+			<?php endif; ?>
+		</div>
+	<?php elseif ( $this->success ) : ?>
+		<div class="updated">
+			<p><?php
+
+				printf(_n('The selected event has been successfully imported.', 'The %d selected events have been successfully imported.', $this->imported_total, $this->plugin_slug ), $this->imported_total);
+
+			?></p>
+		</div>
+	<?php endif; ?>
+
+	<?php if( !empty( $this->errors_images ) ) : ?>
+		<div class="error">
+			<p><strong><?php _e( 'The following errors have occurred during importing images:', $this->plugin_slug ); ?></strong></p>
+			<ul class="admin-list">
+				<?php foreach ( $this->errors_images as $error ) : ?>
+					<li><?php echo $error; ?></li>
+				<?php endforeach; ?>
+			</ul>
+			<p><?php _e( 'Please note that this does not effect importing of associated events unless noted.', $this->plugin_slug ); ?></p>
+		</div>
+	<?php endif; ?>
+
+	<div class="tribe-settings-form">
+
+		<form method="post">
+
+
+			<div>
+				<p><label for="oeticket-import-events-by-id"><?php _e( 'Import events by their oeticket URL:', $this->plugin_slug ); ?></label></p>
+				<textarea id="oeticket-import-events-by-id" name="oeticket-import-events-by-id" rows="5" cols="130"></textarea>
+				<p><span class="description"><?php _e( 'One event URL per line', $this->plugin_slug ); ?></span></p>
+			</div>
+
+			<?php wp_nonce_field( 'oeticket-event-import', 'oeticket-confirm-import' ) ?>
+			<input id="oeticket-event-import-submit" class="button-primary" type="submit" value="<?php _e( 'Import events', $this->plugin_slug ); ?>">
+
+		<form>
+	</div>
 
 </div>
